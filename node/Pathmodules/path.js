19b14050b9e16a7b08f188ac1bca6fdd9188771d
Your Name <shlokjagtap123@gmail.com>
2023-09-16 14:01:36 +0530

    Pathmodules

---
+++ node/Pathmodules/path.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+console.log(path.dirname('/home/shlok / Desktop / Stuff / 1923 / node / Pathmodules / path.js'))
+console.log(path.extname('/home/shlok / Desktop / Stuff / 1923 / node / Pathmodules / path.js'))
+console.log(path.basename('/home/shlok / Desktop / Stuff / 1923 / node / Pathmodules / path.js'))
+
+console.log(path.parse("/home/shlok / Desktop / Stuff / 1923 / node / Pathmodules / path.js"));
