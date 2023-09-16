1a0fd541faa46818f03d73f7a68c29446fd02d02
Your Name <shlokjagtap123@gmail.com>
2023-09-16 15:04:07 +0530

    operators learnt

---
+++ node/npmMod/index.js
@@ -0,0 +1,7 @@
+// const chalk = require("chalk")
+
+// console.log(chalk.blue.italic("hello world"));
+const validator = require("validator");
+const res = validator.isEmail("shlokjagtap.0608@gmail.com")
+console.log(res);
+// console.log(res ? chalk.green.inverse(res) : chalk.red.inverse(res))
