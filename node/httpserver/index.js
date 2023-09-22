4fdd3b8a835e23ee76aebc97bcf86c904dcfd972
Your Name <shlokjagtap123@gmail.com>
2023-09-22 14:50:16 +0530

    page routing

--- node/httpserver/index.js
+++ node/httpserver/index.js
@@ -1,6 +1,18 @@
 const http= require("http");
 const server = http.createServer((req, res) => {
-    res.end('Hello from the other side');
+    // console.log(req.url);
+    if (req.url =="/")
+    {
+        res.end('Hello from the home side');
+    }
+    else if (req.url == "/home")
+    {
+        res.end("Hello from home page")
+    }
+    else
+    {
+        res.end("404 error")
+        }
 }); 
 server.listen(8000, "127.0.0.1", () => {
     console.log("listening to the port no 8000")
