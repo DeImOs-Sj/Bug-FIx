7e372232c3f18e42c11f54ff52e6d783292e2e12
Your Name <shlokjagtap123@gmail.com>
2023-09-26 16:21:46 +0530

    dynamic data

--- Express/src/index.js
+++ Express/src/index.js
@@ -2,7 +2,14 @@ var path = require ("path")
 var express = require('express')
 var app = express();
 const staticPath = path.join(__dirname, "../public")
-app.use(express.static(staticPath));
+
+app.set('view engine', 'hbs');
+app.set('views', '/home/shlok/Desktop/Stuff/1923/Express/views');
+
+// app.use(express.static(staticPath));
+app.get("/", (req, res) => {
+    res.render('index.hbs')
+})
 
  
 console.log(path.join(__dirname,"../public"))
