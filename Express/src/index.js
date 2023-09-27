785b416f963f6a19f24385764b31b8d7089c0ebb
Your Name <shlokjagtap123@gmail.com>
2023-09-27 14:11:45 +0530

    done with express

--- Express/src/index.js
+++ Express/src/index.js
@@ -1,10 +1,17 @@
 var path = require ("path")
 var express = require('express')
+var hbs = require('hbs')
 var app = express();
 const staticPath = path.join(__dirname, "../public")
+const templatePath = path.join(__dirname, "../templates/views")
+const partialsPath = path.join(__dirname, "../templates/partials")
+
 
 app.set('view engine', 'hbs');
-app.set('views', '/home/shlok/Desktop/Stuff/1923/Express/views');
+// app.set('views', '/home/shlok/Desktop/Stuff/1923/Express/vi""ews');
+app.set("views", templatePath)
+
+hbs.registerPartials(partialsPath);
 
 // app.use(express.static(staticPath));
 app.get("/", (req, res) => {
@@ -16,8 +23,15 @@ console.log(path.join(__dirname,"../public"))
 app.get("/", (req, res) => {
     res.send("hello world");
 })
-app.get("/Start", (req, res) => {
-    res.send("hello world");
+app.get("/start", (req, res) => {
+    res.send("this is start page");
+
+})
+
+app.get("*", (req, res) => {
+    res.render("404", {
+        errorcomment: 'oops page not found'
+    })
 })
 app.listen(8000, () => {
     console.log("listining to the port")
