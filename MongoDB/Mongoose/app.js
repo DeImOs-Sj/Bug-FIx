beaa52836ad007a216690d641a418aeb3944cc94
Your Name <shlokjagtap123@gmail.com>
2023-10-02 16:10:50 +0530

    learned various operations

--- MongoDB/Mongoose/app.js
+++ MongoDB/Mongoose/app.js
@@ -3,3 +3,70 @@ mongoose.connect("mongodb://localhost:27017/Shlok", { useNewUrlParser: true, use
     .then(() => console.log("connection successfull"))
     .catch((err)=> console.log(err));
 
+const playlistSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    ctype: String,
+    videos: Number,
+    author: String,
+    active: Boolean,
+    date: {
+        type: Date,
+        default:Date.now
+    }
+})
+
+const Playlist = new mongoose.model("Playlist", playlistSchema)
+
+const createDocument = async () => {
+    try {
+        const reactPlaylist = Playlist({
+            name: "React.js",
+            ctype: "Frontend",
+            videos: 80,
+            author: "shlok",
+            active: true,
+
+        })
+        const jsPlaylist = Playlist({
+            name: "React.js",
+            ctype: "Frontend",
+            videos: 180,
+            author: "shlok",
+            active: true,
+
+        })
+        const mongodbPlaylist = Playlist({
+            name: "Database",
+            ctype: "Backend",
+            videos: 80,
+            author: "shlok",
+            active: true,
+
+        })
+        const mongoosePlaylist = Playlist({
+            name: "mongoose",
+            ctype: "Backend",
+            videos: 88,
+            author: "shlok",
+            active: true,
+
+        })
+        const result = await Playlist.insertMany([reactPlaylist, jsPlaylist, mongodbPlaylist, mongoosePlaylist]);
+        console.log(result)
+    } catch (err) {
+        console.log(err);
+}
+  
+    
+}
+// createDocument();
+const getDocument = async () => {
+    const result = await Playlist.find({videos:{$gte:80}}); 
+    console.log(result)
+}
+getDocument();
+
