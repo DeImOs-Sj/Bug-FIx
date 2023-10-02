eeefda8dd8026815c9b79d417b37dfa3849b20c1
Your Name <shlokjagtap123@gmail.com>
2023-10-02 22:13:54 +0530

    Mongo completed

--- MongoDB/Mongoose/app.js
+++ MongoDB/Mongoose/app.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 mongoose.connect("mongodb://localhost:27017/Shlok", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("connection successfull"))
     .catch((err)=> console.log(err));
@@ -6,12 +7,39 @@ mongoose.connect("mongodb://localhost:27017/Shlok", { useNewUrlParser: true, use
 const playlistSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        lowercase: true,
+        trim: true,
+        minlength: 2,
+        maxlength:30,
     },
 
-    ctype: String,
-    videos: Number,
+    ctype: {
+        type: String,
+        required: true,
+        lowercase: true,
+        enum: ["frontend", "backend", "database"]
+    },
+    videos: {
+        type: Number,
+        validate(value) {
+            if (value < 0)
+            {
+                throw new Error("error no cannot be negative")
+                }
+        }
+    },
     author: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Email is invalid")
+            }
+        }
+    },
     active: Boolean,
     date: {
         type: Date,
@@ -48,14 +76,15 @@ const createDocument = async () => {
 
         })
         const mongoosePlaylist = Playlist({
-            name: "mongoose",
-            ctype: "Backend",
-            videos: 88,
+            name: "js ",
+         ctype:"Database",
+            videos: 8,
             author: "shlok",
+            email:"shlok@gmail.com",
             active: true,
 
         })
-        const result = await Playlist.insertMany([reactPlaylist, jsPlaylist, mongodbPlaylist, mongoosePlaylist]);
+        const result = await Playlist.insertMany([ mongoosePlaylist]);
         console.log(result)
     } catch (err) {
         console.log(err);
@@ -63,10 +92,44 @@ const createDocument = async () => {
   
     
 }
-// createDocument();
+createDocument();
 const getDocument = async () => {
-    const result = await Playlist.find({videos:{$gte:80}}); 
-    console.log(result)
+    const result = await Playlist.find
+        ({ author: "shlok" })
+        .select({ name: 1 })
+        .sort({name:1});
+        // .countDocuments();
+    
+    // console.log(result)
 }
-getDocument();
+// getDocument();
+const updateDocument = async (_id) => {
+    try {
+        const result = await Playlist.findByIdAndUpdate({ _id }, {
+            $set: {
+                author: "Shlok Jagtap"
+            }, 
+                new:true,
+        })
+        console.log(result)
+        
+    } catch (err) {
+        console.log(err)
+        
+    }
+  
+}
+// updateDocument("651a8f5605bffd7e9e7fb1a9")
+const deleteDocument = async (_id) => {
+    try {
+        const result = await Playlist.deleteOne({ _id });
+        console.log(result)
+
+    } catch (err) {
+        console.log(err)
+    }
+    
+}
+
+// deleteDocument("651a8f5605bffd7e9e7fb1a9")
 
