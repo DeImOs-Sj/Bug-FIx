c705939554bb35c62204474fef81eb7a26fc6613
Your Name <shlokjagtap123@gmail.com>
2023-10-04 18:56:21 +0530

    done with restful api

--- Restfulapi/src/app.js
+++ Restfulapi/src/app.js
@@ -10,17 +10,71 @@ const port = process.env.PORT || 3000;
 // })
 app.use(express.json())
 
-app.post("/students", (req, res) => {
-    console.log(req.body)
+// app.post("/students", (req, res) => {
+//     console.log(req.body)
 
-    const user = new Student(req.body)
-    user.save().then(() => {
-        res.send(user);
-    }).catch((e) => {
-        res.send(e)
-    })
-    // res.send("response from this side ")
+//     const user = new Student(req.body)
+//     user.save().then(() => {
+//         res.send(user);
+//     }).catch((e) => {
+//         res.send(e)
+//     })
+//     // res.send("response from this side ")
     
+// })
+//post method
+app.post("/students", async (req, res) => {
+    try {
+        const user = new Student(req.body)
+        const createUser = await user.save();
+        res.status(201).send * (createUser)
+        
+    } catch (e) {
+        res.status(400).send(e)
+
+    }
+   
+   
+    
+})
+//get method
+app.get("/students", async (req, res) => {
+    try {
+        const studentsData = await Student.find();
+        res.send(studentsData)
+
+    } catch (e) {
+        res.status(400).send(e)
+
+    }
+})
+//get by id 
+app.get("/students/:id", async (req, res) => {
+    try {
+        const _id = req.params.id;  
+        const studentData = await Student.findById(_id);
+        console.log(studentData)
+        if (!studentData) {
+            return res.status(404).send();
+        } else {
+            res.send(studentData)
+        }
+    } catch (e) {
+        res.send(e)
+    }
+})
+
+//update
+app.patch("/students/:id", async (req, res) => {
+    try {
+        const _id = req.params.id;  
+        const updateStudents = await Student.findByIdAndUpdate(_id, req.body);
+        res.send(updateStudents)
+        console.log(updateStudents)
+        
+    } catch (e) {
+        res.status(404).send(e)
+    }
 })
 app.listen(port, () => {
     console.log(`connection is successful ${port}`)
